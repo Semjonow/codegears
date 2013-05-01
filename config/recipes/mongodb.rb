f24cd7b6593724185056b66a2050458f4fe00d35
Juri Semjonov <juri.semjonov@gmail.com>
2013-05-01 23:14:20 +0300

    mongo config for production

--- config/recipes/mongodb.rb
+++ config/recipes/mongodb.rb
@@ -2,7 +2,7 @@ namespace :mongodb do
   desc "Install latest stable release of MongoDB"
   task :install, roles: :web do
     run "#{sudo} apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
-    run "#{sudo} echo \"deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen\" | #{sudo} tee -a /etc/apt/sources.list.d/10gen.list"
+    run "echo \"deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen\" | #{sudo} tee -a /etc/apt/sources.list.d/10gen.list"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install mongodb-10gen"
   end
