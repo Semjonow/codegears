0b34036f96a5e8d0e27b170fbfc548ad1c61b9b2
Juri Semjonov <juri.semjonov@gmail.com>
2013-05-02 00:50:46 +0300

    faye conf

--- config/recipes/faye.rb
+++ config/recipes/faye.rb
@@ -1,7 +1,7 @@
-set :faye_pid, "#{current_path}/shared/pids/faye.pid"
+set :faye_pid,    "#{current_path}/shared/pids/faye.pid"
 set :faye_config, "#{current_path}/current/faye.ru"
 
-namespace :faye do
+namespace :faye, :roles => :app do
   desc "Start Faye"
   task :start do
     run "cd #{current_path}/current && bundle exec rackup #{faye_config} -s thin -E production -D --pid #{faye_pid}"
@@ -11,6 +11,6 @@ namespace :faye do
   task :stop do
     run "kill `cat #{faye_pid}` || true"
   end
+  before 'deploy:update_code', 'faye:stop'
+  after 'deploy:finalize_update', 'faye:start'
 end
-before 'deploy:update_code', 'faye:stop'
-after 'deploy:finalize_update', 'faye:start'
