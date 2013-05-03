470e8333a518f720c8880690cfb21779c0b5d00e
Juri Semjonov <juri.semjonov@gmail.com>
2013-05-03 07:49:15 +0300

    faye deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :faye_config, "faye.ru"
 namespace :faye do
   desc "Start Faye"
   task :start do
-    run "cd #{current_path} && bundle exec rackup #{faye_config} -s thin -E production -D --pid #{faye_pid}"
+    run "cd #{current_path} && RAILS_ENV=production bundle exec rackup #{faye_config} -s thin -E production -D --pid #{faye_pid}"
   end
 
   desc "Stop Faye"
